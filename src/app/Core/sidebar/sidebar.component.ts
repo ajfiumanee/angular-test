77dd635acb0c327b083975bbac776163f930fa52
Alberto Junior <alberto.junior@performait.com>
2020-02-12 14:26:07 -0300

    correction in variables SASS

--- src/app/Core/sidebar/sidebar.component.ts
+++ src/app/Core/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, HostBinding, Output, EventEmitter  } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.sass']
 })
 export class SidebarComponent implements OnInit {
+  @Output() toggle = new EventEmitter();
 
   constructor() { }
 
