c25a83d700e39a9e40e7f1d5880b5e54b97e5bc7
Alberto Junior <alberto.junior@performait.com>
2020-02-13 18:20:30 -0300

    vaii

--- src/app/Core/core/sidebar/sidebar.component.ts
+++ src/app/Core/core/sidebar/sidebar.component.ts
@@ -9,32 +9,11 @@ import { AuthService } from 'src/app/Services/auth.service';
 export class SidebarComponent implements OnInit {
   @Output() toggle = new EventEmitter();
   @Input() @HostBinding('class.--open') isSidebarOpen: boolean;
-  private openedMenus = new Set<string>();
 
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
 
-  }
-
-  toggleMenu(menu: string) {
-    if (this.isMenuOpen(menu)) {
-      this.closeMenu(menu);
-    } else {
-      this.openMenu(menu);
-    }
-  }
-
-  isMenuOpen(menu: string) {
-    return this.openedMenus.has(menu);
-  }
-
-  openMenu(menu: string) {
-    this.openedMenus.add(menu);
-  }
-
-  closeMenu(menu: string) {
-    this.openedMenus.delete(menu);
   }
 
   logout() {
