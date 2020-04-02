ccb5f3fd3c81801a114192b45f802ac1272005b9
Alberto.junior <alberto.junior@performait.com>
2020-04-02 11:20:51 -0300

    install jsPDF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PdfTestComponent } from './pdf-test/pdf-test.component';
+import { PdfPuppeteerComponent } from './pdf-puppeteer/pdf-puppeteer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PdfTestComponent,
+    PdfPuppeteerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pdf-puppeteer/pdf-puppeteer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-puppeteer',
+  templateUrl: './pdf-puppeteer.component.html',
+  styleUrls: ['./pdf-puppeteer.component.scss']
+})
+export class PdfPuppeteerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
